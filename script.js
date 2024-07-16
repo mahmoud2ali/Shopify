062e2816481d6953c11e08569e44373000b1aa7e
Mahmoud <mahmoud2m2ali@gmail.com>
2024-07-16 19:01:20 +0300

    done

---
+++ script.js
@@ -0,0 +1,33 @@
+// please show for every product: price before and  after discount, thumbnail, title, description
+
+let url = "https://dummyjson.com/products"
+
+const Data = async ()=> {
+    let response = await fetch(url);
+    let data = await response.json();
+    let products = data.products;
+    return products;
+}
+
+showProducts =async()=>{
+    products= await Data();
+    let productList = document.getElementById("product-list");
+    products.forEach(product => {
+        let proDiv = document.createElement('div');
+        proDiv.className ='card';
+        let finalPrice = product.price - (product.price * product.discountPercentage / 100).toFixed()
+        proDiv.innerHTML = `
+            <img src="${product.thumbnail}" alt="${product.title}">
+            <h2>${product.title}</h2>
+            <p class='oldPirce'>price: $${product.price}</p>
+            <p class='price'>${finalPrice}</p>
+            <p class='description'>${product.description}</p>
+        `;
+        productList.appendChild(proDiv);
+    });
+}
+
+window.onload = showProducts()
+
+
+console.log(Data())
